23ac2ba2ed6e68583268eb4f76d08508cf01f43d
nwalter <nwalter@NWALTER.wjp.de>
2018-03-01 21:41:55 +0100

    Klasse geschrieben

--- HtmlElementBuilder.ts
+++ HtmlElementBuilder.ts
@@ -1,6 +1,28 @@
 export class HtmlElementBuilder {
-    
-    constructor() {
-        
+    htmlMainElement: HTMLElement;
+
+    public constructor(tag: string, attributes: Object) {
+        this.htmlMainElement = document.createElement(tag);
+        for(let key in attributes) {
+            this.addAttribute(key, attributes[key]);
+        }
+    }
+
+    private addAttribute(key: string, attribute: any) {
+        if(key == 'classList') {
+            for(let j of attribute) {
+                this.htmlMainElement.classList.add(j);
+            }
+        } else {
+            this.htmlMainElement[key] = attribute;
+        }
+    }
+
+    public addChild(child: HTMLElement) {
+        this.htmlMainElement.appendChild(child)
+    }
+
+    public get() {
+        return this.htmlMainElement;
     }
 }
