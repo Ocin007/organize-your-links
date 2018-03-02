612bae56801dd38151360d1d4150e614c18bc994
nwalter <nwalter@NWALTER.wjp.de>
2018-03-02 14:47:06 +0100

    HtmlElementBuilder erweitert

--- HtmlElementBuilder.ts
+++ HtmlElementBuilder.ts
@@ -1,14 +1,18 @@
 export class HtmlElementBuilder {
-    htmlMainElement: HTMLElement;
+    private htmlMainElement: HTMLElement;
+    private htmlTag: string;
+    private htmlAttributes: Object;
+    private htmlStyle: Object;
+    private htmlChildElements: HtmlElementBuilder[];
 
-    public constructor(tag: string, attributes: Object) {
-        this.htmlMainElement = document.createElement(tag);
-        for(let key in attributes) {
-            this.addAttribute(key, attributes[key]);
-        }
+    public constructor(tag: string, attributes: Object, style: Object) {
+        this.htmlTag = tag;
+        this.htmlAttributes = attributes;
+        this.htmlStyle = style;
+        this.htmlChildElements = [];
     }
 
-    private addAttribute(key: string, attribute: any) {
+    private addAttributeToBuild(key: string, attribute: any) {
         if(key === 'classList') {
             for(let j of attribute) {
                 this.htmlMainElement.classList.add(j);
@@ -18,11 +22,56 @@ export class HtmlElementBuilder {
         }
     }
 
-    public addChild(child: HTMLElement) {
-        this.htmlMainElement.appendChild(child)
+    private addStyleToBuild(key: string, attribute: string) {
+        this.htmlMainElement.style[key] = attribute;
+    }
+
+    private buildElement() {
+        this.htmlMainElement = document.createElement(this.htmlTag);
+        for(let key in this.htmlAttributes) {
+            this.addAttributeToBuild(key, this.htmlAttributes[key]);
+        }
+        for(let key in this.htmlStyle) {
+            this.addStyleToBuild(key, this.htmlStyle[key]);
+        }
+        for(let child of this.htmlChildElements) {
+            this.htmlMainElement.appendChild(child.get());
+        }
     }
 
     public get() {
+        this.buildElement();
         return this.htmlMainElement;
+    }
+
+    public addChild(child: HtmlElementBuilder) {
+        this.htmlChildElements.push(child);
+    }
+
+    public editAttribute(key: string, value: string) {
+        this.htmlAttributes[key] = value;
+    }
+
+    public editClassList(str: string) {
+        var index = this.htmlAttributes['classList'].indexOf(str);
+        if(index !== -1) {
+            this.htmlAttributes['classList'] = this.removeClassFromClassList(index);
+        } else {
+            this.htmlAttributes['classList'].push(str);
+        }
+    }
+    
+    private removeClassFromClassList(index: number) {
+        var helperArray: string[];
+        for(let i in this.htmlAttributes['classList']) {
+            if(parseInt(i) !== index) {
+                helperArray.push(this.htmlAttributes['classList'][i]);
+            }
+        }
+        return helperArray;
+    }
+
+    public editStyle(key: string, value: string) {
+        this.htmlStyle[key] = value;
     }
 }
